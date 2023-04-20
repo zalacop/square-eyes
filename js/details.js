45034a54b2437cba54e48a512f801441c693f081
zalacop <103320914+zalacop@users.noreply.github.com>
2023-04-20 16:31:14 +0200

    connected to detail page
    and added content to movie page

--- js/details.js
+++ js/details.js
@@ -1,70 +1,152 @@
-// adding a variable to href value, retrieving it from the query string and call it
+// // adding a variable to href value, retrieving it from the query string and call it
+// const queryString = document.location.search;
+// const params = new Proxy(new URLSearchParams(queryString), {
+//     get: function(searchParams, id) {
+//         return searchParams.get(id);
+//     }
+// });
+// const movieName = params.movie;
+
+// console.log(movieName);
+
+
+// // finding the chosen movie 
+// async function findMovie(movieId) {
+//     try {
+//         const response = await fetch("/json/movies.json");
+//         const movies =  await response.json("/json/movies.json");
+//         for(let i = 0; i < movies.length; i++) {
+//             if(movies[i].id === movieId) {
+//                 return movies[i];
+//             }
+//         }
+//     } catch(error) {
+//         console.log(error);
+//     }
+// }
+
+// accessing html content
+const movieContainer = document.querySelector(".movie");
+const movieDescriptionContainer = document.querySelector(".description");
+
+
+// // getting the chosen movie
+// async function getMovie() {
+//     const movie2 = await findMovie(movieName);
+//     console.log(movie2);
+
+//     // adding name property to the title
+//     const newTitle = movie2.name; 
+//     document.title = newTitle;
+
+//     // adding space in properties in an array
+//     const genre = movie2.genre.join(", ");
+//     const cast = movie2.cast.join(", ");
+
+//     // creating html for the chosen movie
+//     movie.innerHTML = `<div class="text">
+//                         <h1>${movie2.name}</h1>
+//                         <div class="details">
+//                             <p>${genre}</p>
+//                             <p>${movie2.year}</p>
+//                             <p>${movie2.time}</p>
+//                         </div>
+//                         <a href="/user/payment.html"><button class="cta log">Buy Now 169 NOK</button></a>
+//                         <div class="movie-icons">
+//                             <div class="my-list"><i class="fa-regular fa-plus"></i></div>
+//                             <div class="star"><i class="fa-regular fa-star"></i></div>
+//                         </div>
+//                       </div>
+//     <div class="poster">
+//         <img src="${movie2.image}" alt="Poster for ${movie2.name}" class="poster-img"/>
+//         <div class="close-icon">
+//         <a href="/index.html"><i class="fa-regular fa-circle-xmark"></i></a>
+//         </div>
+//     </div>`;
+
+//     movieDescription.innerHTML = `<p>${movie2.description}</p>
+//     <p>Cast: ${cast}</p>`;
+// }
+// getMovie();
+
+
+const localHost = "http://localhost:10004";
+const woocommerce = "/wp-json/wc/store";
+const products ="/products/"; 
+
+// adding a variable to href value, retrieving it from the query string and using it in an api call
 const queryString = document.location.search;
-const params = new Proxy(new URLSearchParams(queryString), {
-    get: function(searchParams, id) {
-        return searchParams.get(id);
-    }
-});
-const movieName = params.movie;
+const params = new URLSearchParams(queryString);
+const id = params.get("movie");
 
-console.log(movieName);
+console.log(id);
 
+const url = localHost + woocommerce + products + id;
 
 // finding the chosen movie 
-async function findMovie(movieId) {
+async function findMovie() {
     try {
-        const response = await fetch("/json/movies.json");
-        const movies =  await response.json("/json/movies.json");
-        for(let i = 0; i < movies.length; i++) {
-            if(movies[i].id === movieId) {
-                return movies[i];
+        const response = await fetch(url);
+        const movies =  await response.json();
+        const movieName = movies.name;
+        const movieDescription = movies.description;
+        console.log(movieDescription);
+
+        // adding name property to the title
+        const newTitle = movieName; 
+        document.title = newTitle;
+
+        // creating the genre array and joining it
+        const genres = [movies.categories[0].name, movies.categories[1].name];
+        const genre = genres.join(", ");
+        console.log(genre);
+
+        const attributes = movies.attributes;
+
+        // creating cast, year and time variables to use in making html
+        const cast = findAttributeValue(attributes, "cast");
+        const year = findAttributeValue(attributes, "release year");
+        const time = findAttributeValue(attributes, "time");
+        console.log(time);
+        
+        // getting an array of attributes, to access cast, release year and length of the movie
+        function findAttributeValue(attributes, name) {
+            for (let i = 0; i < attributes.length; i++) {
+                const attribute = attributes[i];
+                if(attribute.name.toLowerCase().includes(name)) {
+                    return attribute.terms[0].name;
+                }
             }
         }
+
+        // creating html for the chosen movie
+        movieContainer.innerHTML = `<div class="text">
+                            <h1>${movieName}</h1>
+                            <div class="details">
+                                <p>${genre}</p>
+                                <p>${year}</p>
+                                <p>${time}</p>
+                            </div>
+                            <a href="/user/payment.html"><button class="cta log">Buy Now 169 NOK</button></a>
+                            <div class="movie-icons">
+                                <div class="my-list"><i class="fa-regular fa-plus"></i></div>
+                                <div class="star"><i class="fa-regular fa-star"></i></div>
+                            </div>
+                        </div>
+        <div class="poster">
+            <img src="${movies.images[0].src}" alt="Poster for ${movieName}" class="poster-img"/>
+            <div class="close-icon">
+            <a href="/index.html"><i class="fa-regular fa-circle-xmark"></i></a>
+            </div>
+        </div>`;
+
+        movieDescriptionContainer.innerHTML = `${movieDescription}
+        <p>Cast: ${cast}</p>`;
+
+
     } catch(error) {
         console.log(error);
     }
 }
 
-// accessing html content
-const movie = document.querySelector(".movie");
-const movieDescription = document.querySelector(".description");
-
-
-// getting the chosen movie
-async function getMovie() {
-    const movie2 = await findMovie(movieName);
-    console.log(movie2);
-
-    // adding name property to the title
-    const newTitle = movie2.name; 
-    document.title = newTitle;
-
-    // adding space in properties in an array
-    const genre = movie2.genre.join(", ");
-    const cast = movie2.cast.join(", ");
-
-    // creating html for the chosen movie
-    movie.innerHTML = `<div class="text">
-                        <h1>${movie2.name}</h1>
-                        <div class="details">
-                            <p>${genre}</p>
-                            <p>${movie2.year}</p>
-                            <p>${movie2.time}</p>
-                        </div>
-                        <a href="/user/payment.html"><button class="cta log">Buy Now 169 NOK</button></a>
-                        <div class="movie-icons">
-                            <div class="my-list"><i class="fa-regular fa-plus"></i></div>
-                            <div class="star"><i class="fa-regular fa-star"></i></div>
-                        </div>
-                      </div>
-    <div class="poster">
-        <img src="${movie2.image}" alt="Poster for ${movie2.name}" class="poster-img"/>
-        <div class="close-icon">
-        <a href="/index.html"><i class="fa-regular fa-circle-xmark"></i></a>
-        </div>
-    </div>`;
-
-    movieDescription.innerHTML = `<p>${movie2.description}</p>
-    <p>Cast: ${cast}</p>`;
-}
-getMovie();
+findMovie();
--- js/index.js
+++ js/index.js
@@ -6,76 +6,14 @@ const comedyContainer = document.querySelector(".comedy");
 const dramaContainer = document.querySelector(".drama");
 const actionContainer = document.querySelector(".action");
 
-// async function getMovies() {
-
-//     // fetching the JSON and catching error message    
-//     try {
-//         const response = await fetch("/json/movies.json");
-//         const movies =  await response.json("/json/movies.json");
-
-//         console.log(movies);
-
-//      //looping list of movies and creating html
-//     for (let i = 0; i < movies.length; i++) {
-//         console.log(movies[i].name)
-
-//         // adding continue watching section
-//         if (movies[i].watching === true) {
-//             continueContainer.innerHTML += `
-//             <div><a href="/films/watch.html"><img src="${movies[i].image}" alt="Poster for ${movies[i].name}" class="continue__img" /></a></div>`;
-//         }
-
-//         // adding my list section 
-//         if (movies[i].myList === true) {
-//             myListContainer.innerHTML += `
-//             <div><a href="/films/movie.html?movie=${movies[i].id}"><img src="${movies[i].image}" alt="Poster for ${movies[i].name}" class="img" /></a></div>`;
-//         }
-    
-//         //filltering movies with specific genre
-//         const genre = movies[i].genre;
-//         const filteredGenre = genre.filter(filterGenre);
-
-//         function filterGenre(movie) {
-//             // adding horror section 
-//             if(movie.toLowerCase().includes("horror")) {
-//                 return horrorContainer.innerHTML += `<div>
-//                     <a href="/films/movie.html?movie=${movies[i].id}"><img src="${movies[i].image}" alt="Poster for ${movies[i].name}" class="img" /></a></div>`;
-//             }
-
-//             // adding comedy section
-//             if(movie.toLowerCase().includes("comedy")) {
-//                 return comedyContainer.innerHTML += `<div>
-//                 <a href="/films/movie.html?movie=${movies[i].id}"><img src="${movies[i].image}" alt="Poster for ${movies[i].name}" class="img" /></a></div>`;
-//             }
-
-//             // adding drama section
-//             if(movie.toLowerCase().includes("drama") && dramaContainer !== null) {
-//                 return dramaContainer.innerHTML += `<div>
-//                 <a href="/films/movie.html?movie=${movies[i].id}"><img src="${movies[i].image}" alt="Poster for ${movies[i].name}" class="img" /></a></div>`;
-//             }
-
-//             // saw that there are not all movies displayed, so i added action section
-//             if(movie.toLowerCase().includes("action") && actionContainer !== null) {
-//                 return actionContainer.innerHTML += `<div>
-//                 <a href="/films/movie.html?movie=${movies[i].id}"><img src="${movies[i].image}" alt="Poster for ${movies[i].name}" class="img" /></a></div>`;
-//             }
-//         }  
-//     } 
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-// getMovies();
-
-
 
 //creating the API url (http://localhost:10004/wp-json/wc/store/products)
 
-const api = "http://localhost:10004";
+const localHost = "http://localhost:10004";
 const woocommerce = "/wp-json/wc/store";
 const products ="/products"; 
 
-const url = api + woocommerce + products;
+const url = localHost + woocommerce + products;
 
 
 async function getMovies() {
@@ -90,16 +28,17 @@ async function getMovies() {
 
     for (let i = 0; i < movies.length; i++) {
         console.log(movies[i].name);
-        // // adding continue watching section
+
+        // adding continue watching section
             continueContainer.innerHTML = `
             <div><a href="/films/watch.html"><img src="${movies[2].images[0].src}" alt="Poster for ${movies[2].name}" class="continue__img" /></a></div>
             <div><a href="/films/watch.html"><img src="${movies[5].images[0].src}" alt="Poster for ${movies[5].name}" class="continue__img" /></a></div>
             <div><a href="/films/watch.html"><img src="${movies[7].images[0].src}" alt="Poster for ${movies[7].name}" class="continue__img" /></a></div>`;
 
-        // // adding my list section 
+        // adding my list section 
             myListContainer.innerHTML = `
-            <div><a href="/films/movie.html?movie=${movies[6].id}"><img src="${movies[6].images[0].src}" alt="Poster for ${movies[6].name}" class="img" /></a></div>
-        <div><a href="/films/movie.html?movie=${movies[3].id}"><img src="${movies[3].images[0].src}" alt="Poster for ${movies[3].name}" class="img" /></a></div>`;
+            <div><a href="/films/movie.html?movie=${movies[0].id}"><img src="${movies[0].images[0].src}" alt="Poster for ${movies[0].name}" class="img" /></a></div>
+            <div><a href="/films/movie.html?movie=${movies[3].id}"><img src="${movies[3].images[0].src}" alt="Poster for ${movies[3].name}" class="img" /></a></div>`;
 
     
         //filltering movies with specific genre
@@ -111,27 +50,29 @@ async function getMovies() {
 
             const filteredGenre = filterGenre(genre, movies[i]);
 
+            
             function filterGenre(genre, movie) {
-                function moviesContainer(genre, movie, container) {
+                // making a reusable function to html content
+                function moviesContainer(movie, container) {
                     return container.innerHTML += `<div>
                     <a href="/films/movie.html?movie=${movie.id}"><img src="${movie.images[0].src}" alt="Poster for ${movie.name}" class="img" /></a></div>`;
                 }
 
                  // adding horror section
                 if(genre.toLowerCase().includes("horror") && horrorContainer !== null) {
-                    return moviesContainer(genre, movie, horrorContainer);
+                    return moviesContainer(movie, horrorContainer);
                 }
                 // adding comedy section
                 if(genre.toLowerCase().includes("comedy") && comedyContainer !== null) {
-                    return moviesContainer(genre, movie, comedyContainer);
+                    return moviesContainer(movie, comedyContainer);
                 }
                 // adding drama section
                 if(genre.toLowerCase().includes("drama") && dramaContainer !== null) {
-                    return moviesContainer(genre, movie, dramaContainer);
+                    return moviesContainer(movie, dramaContainer);
                 }
 
                 if(genre.toLowerCase().includes("action") && actionContainer !== null) {
-                    return moviesContainer(genre, movie, actionContainer);
+                    return moviesContainer(movie, actionContainer);
                 }
             }  
         }
