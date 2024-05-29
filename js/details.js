cc14fe4b9bc318496153e0a75a2134bf7cb0421c
zalacop <103320914+zalacop@users.noreply.github.com>
2024-05-29 23:02:20 +0200

    fetching from json file

--- js/details.js
+++ js/details.js
@@ -1,169 +1,174 @@
-// // adding a variable to href value, retrieving it from the query string and call it
-// const queryString = document.location.search;
-// const params = new Proxy(new URLSearchParams(queryString), {
-//     get: function(searchParams, id) {
-//         return searchParams.get(id);
-//     }
-// });
-// const movieName = params.movie;
+const queryString = document.location.search;
+const params = new Proxy(new URLSearchParams(queryString), {
+    get: function(searchParams, id) {
+        return searchParams.get(id);
+    }
+});
+const movieName = params.movie;
 
-// console.log(movieName);
+console.log(movieName);
 
 
-// // finding the chosen movie 
-// async function findMovie(movieId) {
-//     try {
-//         const response = await fetch("/json/movies.json");
-//         const movies =  await response.json("/json/movies.json");
-//         for(let i = 0; i < movies.length; i++) {
-//             if(movies[i].id === movieId) {
-//                 return movies[i];
-//             }
-//         }
-//     } catch(error) {
-//         console.log(error);
-//     }
-// }
+// finding the chosen movie 
+async function findMovie(movieId) {
+    try {
+        const response = await fetch("/json/movies.json");
+        const movies = await response.json();
+        for (let i = 0; i < movies.length; i++) {
+            if (movies[i].id === movieId) {
+                return movies[i];
+            }
+        }
+        throw new Error("Movie not found");
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 // accessing html content
 const movieContainer = document.querySelector(".movie");
 const movieDescriptionContainer = document.querySelector(".description");
 
-
-// // getting the chosen movie
-// async function getMovie() {
-//     const movie2 = await findMovie(movieName);
-//     console.log(movie2);
-
-//     // adding name property to the title
-//     const newTitle = movie2.name; 
-//     document.title = newTitle;
-
-//     // adding space in properties in an array
-//     const genre = movie2.genre.join(", ");
-//     const cast = movie2.cast.join(", ");
-
-//     // creating html for the chosen movie
-//     movie.innerHTML = `<div class="text">
-//                         <h1>${movie2.name}</h1>
-//                         <div class="details">
-//                             <p>${genre}</p>
-//                             <p>${movie2.year}</p>
-//                             <p>${movie2.time}</p>
-//                         </div>
-//                         <a href="/user/payment.html"><button class="cta log">Buy Now 169 NOK</button></a>
-//                         <div class="movie-icons">
-//                             <div class="my-list"><i class="fa-regular fa-plus"></i></div>
-//                             <div class="star"><i class="fa-regular fa-star"></i></div>
-//                         </div>
-//                       </div>
-//     <div class="poster">
-//         <img src="${movie2.image}" alt="Poster for ${movie2.name}" class="poster-img"/>
-//         <div class="close-icon">
-//         <a href="/index.html"><i class="fa-regular fa-circle-xmark"></i></a>
-//         </div>
-//     </div>`;
-
-//     movieDescription.innerHTML = `<p>${movie2.description}</p>
-//     <p>Cast: ${cast}</p>`;
-// }
-// getMovie();
-
-
-const localHost = "https://www.square-eyes-z91.no/";
-const woocommerce = "/wp-json/wc/store";
-const products = "/products"; 
-const consumerKey = "ck_68bcdb5809a7ef0e264c3fb000e5a203fa28f662";
-const consumerSecret = "cs_57a55410e81b8cb6da07582ba6a508627ebbc75e";
-
-// adding a variable to href value, retrieving it from the query string and using it in an api call
-const queryString = document.location.search;
-const params = new URLSearchParams(queryString);
-const id = params.get("movie");
-
-console.log(id);
-
-const url = localHost + woocommerce + products + "/" + id + "?" + consumerKey+ "&" + consumerSecret;
-
-// finding the chosen movie 
-async function findMovie() {
-    try {
-        const response = await fetch(url);
-        const movies =  await response.json();
-        const movieName = movies.name;
-        const movieDescription = movies.description;
-        console.log(movieDescription);
-
-        // adding name property to the title and fixed encoding 
-        const title = movieName;
-        function removeOuterTags(inputString) {
-            const parser = new DOMParser();
-            const getTitle = parser.parseFromString(inputString, "text/html");
-            return getTitle.body.innerText;
-        }
-
-        const inputString = `<p>${title}</p>`;
-        const newTitle = removeOuterTags(inputString);
-        console.log(newTitle);
-
-        document.title = newTitle;
-
-        // creating the genre array and joining it
-        const genres = [movies.categories[0].name, movies.categories[1].name];
-        const genre = genres.join(", ");
-        console.log(genre);
-
-        // getting the price 
-        const price = movies.prices.price/100;
-        console.log(price);
-
-        const attributes = movies.attributes;
-
-        // creating cast, year and time variables to use in making html
-        const cast = findAttributeValue(attributes, "cast");
-        const year = findAttributeValue(attributes, "release year");
-        const time = findAttributeValue(attributes, "time");
-        console.log(time);
-        
-        // getting an array of attributes, to access cast, release year and length of the movie
-        function findAttributeValue(attributes, name) {
-            for (let i = 0; i < attributes.length; i++) {
-                const attribute = attributes[i];
-                if(attribute.name.toLowerCase().includes(name)) {
-                    return attribute.terms[0].name;
-                }
-            }
-        }
-
-        // creating html for the chosen movie
-        movieContainer.innerHTML = `<div class="text">
-                            <h1>${movieName}</h1>
-                            <div class="details">
-                                <p>${genre}</p>
-                                <p>${year}</p>
-                                <p>${time}</p>
-                            </div>
-                            <a href="/user/payment.html"><button class="cta log">Buy Now ${price} NOK</button></a>
-                            <div class="movie-icons">
-                                <div class="my-list"><i class="fa-regular fa-plus"></i></div>
-                                <div class="star"><i class="fa-regular fa-star"></i></div>
-                            </div>
-                        </div>
-        <div class="poster">
-            <img src="${movies.images[0].src}" alt="Poster for ${movieName}" class="poster-img"/>
-            <div class="close-icon">
-            <a href="/index.html"><i class="fa-regular fa-circle-xmark"></i></a>
-            </div>
-        </div>`;
-
-        movieDescriptionContainer.innerHTML = `${movieDescription}
-        <p>Cast: ${cast}</p>`;
-
-
-    } catch(error) { // catching if an error occours
-        console.log(error);
-        movieContainer.innerHTML = errorMessage(); // displaying error message
+// getting the chosen movie
+async function getMovie() {
+    const movie = await findMovie(movieName);
+    if (!movie) {
+        console.log("Movie not found");
+        return;
     }
-}
 
-findMovie();
+    console.log(movie);
+
+    // adding name property to the title
+    const newTitle = movie.name; 
+    document.title = newTitle;
+
+    // adding space in properties in an array
+    const genre = movie.genre.join(", ");
+    const cast = movie.cast.join(", ");
+
+    // creating html for the chosen movie
+    movieContainer.innerHTML = `<div class="text">
+                        <h1>${movie.name}</h1>
+                        <div class="details">
+                            <p>${genre}</p>
+                            <p>${movie.year}</p>
+                            <p>${movie.time}</p>
+                        </div>
+                        <a href="/user/payment.html"><button class="cta log">Buy Now 169 NOK</button></a>
+                        <div class="movie-icons">
+                            <div class="my-list"><i class="fa-regular fa-plus"></i></div>
+                            <div class="star"><i class="fa-regular fa-star"></i></div>
+                        </div>
+                      </div>
+    <div class="poster">
+        <img src="${movie.image}" alt="Poster for ${movie.name}" class="poster-img"/>
+        <div class="close-icon">
+        <a href="/index.html"><i class="fa-regular fa-circle-xmark"></i></a>
+        </div>
+    </div>`;
+
+    movieDescriptionContainer.innerHTML = `<p>${movie.description}</p>
+    <p>Cast: ${cast}</p>`;
+}
+getMovie();
+
+
+
+// const localHost = "https://www.square-eyes-z91.no/";
+// const woocommerce = "/wp-json/wc/store";
+// const products = "/products"; 
+// const consumerKey = "ck_68bcdb5809a7ef0e264c3fb000e5a203fa28f662";
+// const consumerSecret = "cs_57a55410e81b8cb6da07582ba6a508627ebbc75e";
+
+// // adding a variable to href value, retrieving it from the query string and using it in an api call
+// const queryString = document.location.search;
+// const params = new URLSearchParams(queryString);
+// const id = params.get("movie");
+
+// console.log(id);
+
+// const url = localHost + woocommerce + products + "/" + id + "?" + consumerKey+ "&" + consumerSecret;
+
+// // finding the chosen movie 
+// async function findMovie() {
+//     try {
+//         const response = await fetch(url);
+//         const movies =  await response.json();
+//         const movieName = movies.name;
+//         const movieDescription = movies.description;
+//         console.log(movieDescription);
+
+//         // adding name property to the title and fixed encoding 
+//         const title = movieName;
+//         function removeOuterTags(inputString) {
+//             const parser = new DOMParser();
+//             const getTitle = parser.parseFromString(inputString, "text/html");
+//             return getTitle.body.innerText;
+//         }
+
+//         const inputString = `<p>${title}</p>`;
+//         const newTitle = removeOuterTags(inputString);
+//         console.log(newTitle);
+
+//         document.title = newTitle;
+
+//         // creating the genre array and joining it
+//         const genres = [movies.categories[0].name, movies.categories[1].name];
+//         const genre = genres.join(", ");
+//         console.log(genre);
+
+//         // getting the price 
+//         const price = movies.prices.price/100;
+//         console.log(price);
+
+//         const attributes = movies.attributes;
+
+//         // creating cast, year and time variables to use in making html
+//         const cast = findAttributeValue(attributes, "cast");
+//         const year = findAttributeValue(attributes, "release year");
+//         const time = findAttributeValue(attributes, "time");
+//         console.log(time);
+        
+//         // getting an array of attributes, to access cast, release year and length of the movie
+//         function findAttributeValue(attributes, name) {
+//             for (let i = 0; i < attributes.length; i++) {
+//                 const attribute = attributes[i];
+//                 if(attribute.name.toLowerCase().includes(name)) {
+//                     return attribute.terms[0].name;
+//                 }
+//             }
+//         }
+
+//         // creating html for the chosen movie
+//         movieContainer.innerHTML = `<div class="text">
+//                             <h1>${movieName}</h1>
+//                             <div class="details">
+//                                 <p>${genre}</p>
+//                                 <p>${year}</p>
+//                                 <p>${time}</p>
+//                             </div>
+//                             <a href="/user/payment.html"><button class="cta log">Buy Now ${price} NOK</button></a>
+//                             <div class="movie-icons">
+//                                 <div class="my-list"><i class="fa-regular fa-plus"></i></div>
+//                                 <div class="star"><i class="fa-regular fa-star"></i></div>
+//                             </div>
+//                         </div>
+//         <div class="poster">
+//             <img src="${movies.images[0].src}" alt="Poster for ${movieName}" class="poster-img"/>
+//             <div class="close-icon">
+//             <a href="/index.html"><i class="fa-regular fa-circle-xmark"></i></a>
+//             </div>
+//         </div>`;
+
+//         movieDescriptionContainer.innerHTML = `${movieDescription}
+//         <p>Cast: ${cast}</p>`;
+
+
+//     } catch(error) { // catching if an error occours
+//         console.log(error);
+//         movieContainer.innerHTML = errorMessage(); // displaying error message
+//     }
+// }
+
+// findMovie();
--- js/index.js
+++ js/index.js
@@ -1,4 +1,132 @@
-// accessing html content
+// // accessing html content
+// const continueContainer = document.querySelector(".continue");
+// const myListContainer = document.querySelector(".my-list");
+// const horrorContainer = document.querySelector(".horror");
+// const comedyContainer = document.querySelector(".comedy");
+// const dramaContainer = document.querySelector(".drama");
+// const actionContainer = document.querySelector(".action");
+// const promotionsContainer = document.querySelector(".images");
+// const promotionsNavContainer = document.querySelector(".navigation-manual");
+
+// // accessing html content on movie page 
+// const nextContainer = document.querySelector(".what_next");
+
+// //creating the API url (http://localhost:10004/wp-json/wc/store/products)
+// // https://www.square-eyes-z91.no/
+
+// const localHost = "https://www.square-eyes-z91.no/";
+// const woocommerce = "/wp-json/wc/store";
+// const products = "/products"; 
+// const consumerKey = "ck_68bcdb5809a7ef0e264c3fb000e5a203fa28f662";
+// const consumerSecret = "cs_57a55410e81b8cb6da07582ba6a508627ebbc75e";
+// const featured = "?featured=true";
+
+// const url = localHost + woocommerce + products + "?" + consumerKey+ "&" + consumerSecret;
+// const featuredURL = localHost + woocommerce + products + featured + "&" + consumerKey + "&" + consumerSecret; 
+
+// async function getMovies() {
+
+//    // fetching the JSON and catching error message
+   
+//    try {
+
+//     // adding featured section
+//     const featuredResonse = await fetch(featuredURL);
+//     const featuredMovies = await featuredResonse.json();
+//     console.log(featuredMovies);
+
+//     if(featuredMovies.length > 0 && promotionsContainer !== null) {
+//         for (let f = 0; f < featuredMovies.length; f++) {
+//             console.log(featuredMovies[f].name);
+    
+//             // adding featured/promotions html
+//             promotionsContainer.innerHTML = `<input type="radio" name="slide" id="image1" checked>
+//             <input type="radio" name="slide" id="image2">
+//             <input type="radio" name="slide" id="image3">
+            
+//             <img src="${featuredMovies[0].images[0].src}" alt="Poster for ${featuredMovies[0].name}" class="img1" />
+//             <img src="${featuredMovies[1].images[0].src}" alt="Poster for ${featuredMovies[1].name}" class="img2" />
+//             <img src="${featuredMovies[2].images[0].src}" alt="Poster for ${featuredMovies[2].name}" class="img3" />`
+            
+//             promotionsNavContainer.innerHTML = `<label for="image1"></label>
+//             <label for="image2"></label>
+//             <label for="image3"></label>`;
+//         }
+//     }
+
+//     // adding content to home page
+//     const response = await fetch(url);
+//     const movies = await response.json();
+
+//     console.log(movies);
+
+
+
+//     for (let i = 0; i < movies.length; i++) {
+//         console.log(movies[i].name);
+
+//         // adding continue watching section
+//             continueContainer.innerHTML = `
+//             <div><img src="${movies[2].images[0].src}" alt="Poster for ${movies[2].name}" class="continue__img" /></div>
+//             <div><img src="${movies[5].images[0].src}" alt="Poster for ${movies[5].name}" class="continue__img" /></div>
+//             <div><img src="${movies[7].images[0].src}" alt="Poster for ${movies[7].name}" class="continue__img" /></div>`;
+
+//         // adding my list section 
+//             myListContainer.innerHTML = `
+//             <div><a href="/films/movie.html?movie=${movies[0].id}"><img src="${movies[0].images[0].src}" alt="Poster for ${movies[0].name}" class="img" /></a></div>
+//             <div><a href="/films/movie.html?movie=${movies[3].id}"><img src="${movies[3].images[0].src}" alt="Poster for ${movies[3].name}" class="img" /></a></div>`;
+
+        
+//         //filltering movies with specific genre
+//         const categories = movies[i].categories;
+
+//         for (let j = 0; j < categories.length; j++) {
+//             const genre = categories[j].name;
+//             console.log(genre);
+
+//             const filteredGenre = filterGenre(genre, movies[i]);
+
+            
+//             function filterGenre(genre, movie) {
+//                 // making a reusable function to html content
+//                 function moviesContainer(movie, container) {
+//                     return container.innerHTML += `<div>
+//                     <a href="/films/movie.html?movie=${movie.id}"><img src="${movie.images[0].src}" alt="Poster for ${movie.name}" class="img" /></a></div>`;
+//                 }
+
+//                  // adding horror section
+//                 if(genre.toLowerCase().includes("horror") && horrorContainer !== null) {
+//                     return moviesContainer(movie, horrorContainer);
+//                 }
+//                 // adding comedy section
+//                 if(genre.toLowerCase().includes("comedy") && comedyContainer !== null) {
+//                     return moviesContainer(movie, comedyContainer);
+//                 }
+//                 // adding drama section
+//                 if(genre.toLowerCase().includes("drama") && dramaContainer !== null) {
+//                     return moviesContainer(movie, dramaContainer);
+//                 }
+
+//                 if(genre.toLowerCase().includes("action") && actionContainer !== null) {
+//                     return moviesContainer(movie, actionContainer);
+//                 }
+//             }  
+//         }
+  
+//     }
+//    } catch(error) { // catching if an error occours
+//     console.log(error);
+//     continueContainer.innerHTML = errorMessage();// displaying error message
+//     }
+// }
+
+// getMovies();
+
+
+
+  
+
+// Accessing HTML content
 const continueContainer = document.querySelector(".continue");
 const myListContainer = document.querySelector(".my-list");
 const horrorContainer = document.querySelector(".horror");
@@ -8,121 +136,74 @@ const actionContainer = document.querySelector(".action");
 const promotionsContainer = document.querySelector(".images");
 const promotionsNavContainer = document.querySelector(".navigation-manual");
 
-// accessing html content on movie page 
-const nextContainer = document.querySelector(".what_next");
-
-//creating the API url (http://localhost:10004/wp-json/wc/store/products)
-// https://www.square-eyes-z91.no/
-
-const localHost = "https://www.square-eyes-z91.no/";
-const woocommerce = "/wp-json/wc/store";
-const products = "/products"; 
-const consumerKey = "ck_68bcdb5809a7ef0e264c3fb000e5a203fa28f662";
-const consumerSecret = "cs_57a55410e81b8cb6da07582ba6a508627ebbc75e";
-const featured = "?featured=true";
-
-const url = localHost + woocommerce + products + "?" + consumerKey+ "&" + consumerSecret;
-const featuredURL = localHost + woocommerce + products + featured + "&" + consumerKey + "&" + consumerSecret; 
+// Local JSON file paths
+const localJSONPath = "/json/movies.json";
 
 async function getMovies() {
+    try {
+        // Fetching all movies
+        const response = await fetch(localJSONPath);
+        const movies = await response.json();
+        console.log(movies);
 
-   // fetching the JSON and catching error message
-   
-   try {
+        // Handling featured movies (assuming the first three are featured)
+        const featuredMovies = movies.slice(0, 3);
+        if (featuredMovies.length > 0 && promotionsContainer !== null) {
+            promotionsContainer.innerHTML = `
+                <input type="radio" name="slide" id="image1" checked>
+                <input type="radio" name="slide" id="image2">
+                <input type="radio" name="slide" id="image3">
+                
+                <img src="${featuredMovies[0].image}" alt="Poster for ${featuredMovies[0].name}" class="img1" />
+                <img src="${featuredMovies[1].image}" alt="Poster for ${featuredMovies[1].name}" class="img2" />
+                <img src="${featuredMovies[2].image}" alt="Poster for ${featuredMovies[2].name}" class="img3" />`;
 
-    // adding featured section
-    const featuredResonse = await fetch(featuredURL);
-    const featuredMovies = await featuredResonse.json();
-    console.log(featuredMovies);
-
-    if(featuredMovies.length > 0 && promotionsContainer !== null) {
-        for (let f = 0; f < featuredMovies.length; f++) {
-            console.log(featuredMovies[f].name);
-    
-            // adding featured/promotions html
-            promotionsContainer.innerHTML = `<input type="radio" name="slide" id="image1" checked>
-            <input type="radio" name="slide" id="image2">
-            <input type="radio" name="slide" id="image3">
-            
-            <img src="${featuredMovies[0].images[0].src}" alt="Poster for ${featuredMovies[0].name}" class="img1" />
-            <img src="${featuredMovies[1].images[0].src}" alt="Poster for ${featuredMovies[1].name}" class="img2" />
-            <img src="${featuredMovies[2].images[0].src}" alt="Poster for ${featuredMovies[2].name}" class="img3" />`
-            
-            promotionsNavContainer.innerHTML = `<label for="image1"></label>
-            <label for="image2"></label>
-            <label for="image3"></label>`;
+            promotionsNavContainer.innerHTML = `
+                <label for="image1"></label>
+                <label for="image2"></label>
+                <label for="image3"></label>`;
         }
-    }
 
-    // adding content to home page
-    const response = await fetch(url);
-    const movies = await response.json();
-
-    console.log(movies);
-
-
-
-    for (let i = 0; i < movies.length; i++) {
-        console.log(movies[i].name);
-
-        // adding continue watching section
-            continueContainer.innerHTML = `
-            <div><img src="${movies[2].images[0].src}" alt="Poster for ${movies[2].name}" class="continue__img" /></div>
-            <div><img src="${movies[5].images[0].src}" alt="Poster for ${movies[5].name}" class="continue__img" /></div>
-            <div><img src="${movies[7].images[0].src}" alt="Poster for ${movies[7].name}" class="continue__img" /></div>`;
-
-        // adding my list section 
-            myListContainer.innerHTML = `
-            <div><a href="/films/movie.html?movie=${movies[0].id}"><img src="${movies[0].images[0].src}" alt="Poster for ${movies[0].name}" class="img" /></a></div>
-            <div><a href="/films/movie.html?movie=${movies[3].id}"><img src="${movies[3].images[0].src}" alt="Poster for ${movies[3].name}" class="img" /></a></div>`;
-
-        
-        //filltering movies with specific genre
-        const categories = movies[i].categories;
-
-        for (let j = 0; j < categories.length; j++) {
-            const genre = categories[j].name;
-            console.log(genre);
-
-            const filteredGenre = filterGenre(genre, movies[i]);
-
-            
-            function filterGenre(genre, movie) {
-                // making a reusable function to html content
-                function moviesContainer(movie, container) {
-                    return container.innerHTML += `<div>
-                    <a href="/films/movie.html?movie=${movie.id}"><img src="${movie.images[0].src}" alt="Poster for ${movie.name}" class="img" /></a></div>`;
-                }
-
-                 // adding horror section
-                if(genre.toLowerCase().includes("horror") && horrorContainer !== null) {
-                    return moviesContainer(movie, horrorContainer);
-                }
-                // adding comedy section
-                if(genre.toLowerCase().includes("comedy") && comedyContainer !== null) {
-                    return moviesContainer(movie, comedyContainer);
-                }
-                // adding drama section
-                if(genre.toLowerCase().includes("drama") && dramaContainer !== null) {
-                    return moviesContainer(movie, dramaContainer);
-                }
-
-                if(genre.toLowerCase().includes("action") && actionContainer !== null) {
-                    return moviesContainer(movie, actionContainer);
-                }
-            }  
+        // Populate the "Continue Watching" section
+        const continueWatching = movies.filter(movie => movie.watching);
+        if (continueWatching.length > 0 && continueContainer !== null) {
+            continueContainer.innerHTML = continueWatching.map(movie => `
+                <div><img src="${movie.image}" alt="Poster for ${movie.name}" class="continue__img" /></div>
+            `).join('');
         }
-  
-    }
-   } catch(error) { // catching if an error occours
-    console.log(error);
-    continueContainer.innerHTML = errorMessage();// displaying error message
+
+        // Populate the "My List" section
+        const myList = movies.filter(movie => movie.myList);
+        if (myList.length > 0 && myListContainer !== null) {
+            myListContainer.innerHTML = myList.map(movie => `
+                <div><a href="/films/movie.html?movie=${movie.id}"><img src="${movie.image}" alt="Poster for ${movie.name}" class="img" /></a></div>
+            `).join('');
+        }
+
+        // Filtering movies by genre and populating corresponding sections
+        movies.forEach(movie => {
+            movie.genre.forEach(genre => {
+                if (genre.toLowerCase() === 'horror' && horrorContainer !== null) {
+                    horrorContainer.innerHTML += `
+                        <div><a href="/films/movie.html?movie=${movie.id}"><img src="${movie.image}" alt="Poster for ${movie.name}" class="img" /></a></div>`;
+                } else if (genre.toLowerCase() === 'comedy' && comedyContainer !== null) {
+                    comedyContainer.innerHTML += `
+                        <div><a href="/films/movie.html?movie=${movie.id}"><img src="${movie.image}" alt="Poster for ${movie.name}" class="img" /></a></div>`;
+                } else if (genre.toLowerCase() === 'drama' && dramaContainer !== null) {
+                    dramaContainer.innerHTML += `
+                        <div><a href="/films/movie.html?movie=${movie.id}"><img src="${movie.image}" alt="Poster for ${movie.name}" class="img" /></a></div>`;
+                } else if (genre.toLowerCase() === 'action' && actionContainer !== null) {
+                    actionContainer.innerHTML += `
+                        <div><a href="/films/movie.html?movie=${movie.id}"><img src="${movie.image}" alt="Poster for ${movie.name}" class="img" /></a></div>`;
+                }
+            });
+        });
+    } catch (error) {
+        console.log(error);
+        if (continueContainer !== null) {
+            continueContainer.innerHTML = `<p>Something went wrong. Please try again later.</p>`;
+        }
     }
 }
 
 getMovies();
-
-
-
-  
-
